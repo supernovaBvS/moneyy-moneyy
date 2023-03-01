e462d9c0eceeb200fd53360495ea90189312d571
supernovaBvS <kongson14@gmail.com>
2023-03-01 11:11:43 +0800

    first commit

---
+++ web/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "transactionss.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'idasjifodafg'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/transaction'
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
